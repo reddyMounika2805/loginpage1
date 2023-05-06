9d6e53b32b5a6a5f43983c0ebad43c18c6de93dd
mounika <reddymounika2805@gmail.com>
2023-05-06 19:23:10 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function LandingPage() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userName, setUserName] = useState('');
+
+  function handleLogin(username, password) {
+   
+    setLoggedIn(true);
+    setUserName(username);
+  }
+
+  function handleLogout() {
+   
+  
+    setLoggedIn(false);
+    setUserName('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loggedIn ? (
+        <p>Welcome back, {userName}!</p>
+      ) : (
+        <div>
+          <p>Welcome, guest user!</p>
+          <LoginForm onLogin={handleLogin} />
+        </div>
+      )}
+
+      {loggedIn && (
+        <button onClick={handleLogout}>Logout</button>
+      )}
     </div>
   );
 }
 
-export default App;
+function LoginForm(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.onLogin(username, password);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Username" value={username} onChange={(event) => setUsername(event.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)} />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default LandingPage;
